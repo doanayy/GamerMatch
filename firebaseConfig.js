b53c2bfaee046cb77ff2d906c51257d1c9a595d4
doanayy <135826372+doanayy@users.noreply.github.com>
2025-04-03 05:00:50 +0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,22 +1,19 @@
+// firebaseConfig.js
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-
 
 const firebaseConfig = {
   apiKey: "AIzaSyAUBjVgjh5z9Ci3YdWfKB8BrMaFL91-G10s",
   authDomain: "gamermatch-59301.firebaseapp.com",
   projectId: "gamermatch-59301",
   storageBucket: "gamermatch-59301.appspot.com",
-  messagingSenderId: "147830673615",
-  appId: "1:147830673615:web:6983afe409a3ea8da727ba",
-  measurementId: "G-9EBQHF6Q5F" 
+  messagingSenderId: "1478380673615",
+  appId: "1:1478380673615:web:6938afe409a3ea8da727ba",
+  measurementId: "G-9EBQHF6Q5F"
 };
 
-
 const app = initializeApp(firebaseConfig);
-
-
 const auth = getAuth(app);
 const db = getFirestore(app);
 
