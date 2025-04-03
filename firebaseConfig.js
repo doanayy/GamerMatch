b72a4458fd88456e027b83a97a09adba38edede9
doanayy <135826372+doanayy@users.noreply.github.com>
2025-04-03 04:42:03 +0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,18 +1,19 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore'; // 🔥 Firestore'u ekliyoruz
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAUBVJgJh52Cj3VdkfF88rMaFL91-G1Os",
+  apiKey: "AIzaSyAUBVJghJc52Gj3VdKf8R8rMaFLg1-G1Os",
   authDomain: "gamermatch-59301.firebaseapp.com",
   projectId: "gamermatch-59301",
   storageBucket: "gamermatch-59301.appspot.com",
-  messagingSenderId: "147803673615",
-  appId: "1:147803673615:web:6938af4e093ea38da277ba"
+  messagingSenderId: "147830673615",
+  appId: "1:147830673615:web:6938afe409a3ea8da727ba",
+  measurementId: "G-9EBQHF6Q5F" 
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app); // 🔥 Burada Firestore'u başlatıyoruz
+const db = getFirestore(app);
 
-export { auth, db }; // ✅ Artık hem auth hem db dışa aktarılıyor
+export { auth, db };
