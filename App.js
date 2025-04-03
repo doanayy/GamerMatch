f643ba15ff3147c342794f9feca4d60f1edc1a81
doanayy <135826372+doanayy@users.noreply.github.com>
2025-04-03 03:46:12 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
-import MatchScreen from './MatchScreen';
 import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { onAuthStateChanged } from 'firebase/auth';
+import { registerRootComponent } from 'expo';
 import { auth, db } from './firebaseConfig';
 import { doc, getDoc } from 'firebase/firestore';
-import { registerRootComponent } from 'expo';
 
 // Ekranlar
 import LoginScreen from './LoginScreen';
@@ -20,14 +18,14 @@ function App() {
   const [isOnboarded, setIsOnboarded] = useState(null); // null = loading
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
       setUser(user);
       if (user) {
         const userDoc = await getDoc(doc(db, 'users', user.uid));
         const data = userDoc.exists() ? userDoc.data() : {};
-        setIsOnboarded(data?.isOnboarded || false);
+        setIsOnboarded(data.isOnboarded || false);
       } else {
-        setIsOnboarded(null); // Kullanıcı çıkış yapınca sıfırla
+        setIsOnboarded(null); // logout olunca sıfırla
       }
     });
 
@@ -37,15 +35,17 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {!user ? (
+        {user ? (
+          isOnboarded === false ? (
+            <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+          ) : (
+            <Stack.Screen name="Match" component={MatchScreen} />
+          )
+        ) : (
           <>
             <Stack.Screen name="Login" component={LoginScreen} />
             <Stack.Screen name="Signup" component={SignUpScreen} />
           </>
-        ) : isOnboarded === false ? (
-          <Stack.Screen name="Onboarding" component={OnboardingScreen} />
-        ) : (
-          <Stack.Screen name="Match" component={MatchScreen} />
         )}
       </Stack.Navigator>
     </NavigationContainer>
